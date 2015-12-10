ced6cceeed00f2527cf990a29ce429ca9328cd05
Vitaly Lobchuk <vn.lobchuk@gmail.com>
2015-12-10 22:24:01 +0300

    Reformat

--- test/app.js
+++ test/app.js
@@ -6,10 +6,8 @@ import {Application} from 'flaky/Application';
 import {FooController} from './fixtures/FooController';
 import {FooComponent} from './fixtures/FooComponent';
 import {FooService} from './fixtures/FooService';
-
 import {FooFilter} from './fixtures/FooFilter';
 import {FooFilterService} from './fixtures/FooFilter';
-
 import {FooDirective} from './fixtures/FooDirective';
 
 let app = new Application('flakyApp');
