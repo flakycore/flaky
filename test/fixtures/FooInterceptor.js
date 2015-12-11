82a8b3a6f7dc3df9f6d300d72ca5e247ad99e394
Vasily Pikulev <54dnya@gmail.com>
2015-12-11 07:13:00 +0300

    Interceptors tests

---
+++ test/fixtures/FooInterceptor.js
@@ -0,0 +1,83 @@
+import {inject, interceptor, service} from 'src/decorators';
+
+@inject('$fooInterceptorService')
+@interceptor('response')
+export class FooResponseInterceptor{
+
+  constructor(fooInterceptorService) {
+    this.fooInterceptorService = fooInterceptorService;
+    this.objToExtend = {fromService: this.fooInterceptorService.getField()};
+  }
+
+  run(config) {
+    return Object.assign(config, this.objToExtend);
+  }
+}
+
+@inject('$fooInterceptorService')
+@interceptor('request')
+export class FooRequestInterceptor{
+
+  constructor(fooInterceptorService) {
+    this.fooInterceptorService = fooInterceptorService;
+    this.objToExtend = {fromService: this.fooInterceptorService.getField()};
+  }
+
+  run(config) {
+    return Object.assign(config, this.objToExtend);
+  }
+}
+
+@inject('$fooInterceptorService')
+@interceptor('responseError')
+export class FooResponseErrorInterceptor{
+
+  constructor(fooInterceptorService) {
+    this.fooInterceptorService = fooInterceptorService;
+    this.objToExtend = {fromService: this.fooInterceptorService.getField()};
+  }
+
+  run(config) {
+    return Object.assign(config, this.objToExtend);
+  }
+}
+
+@inject('$fooInterceptorService')
+@interceptor('requestError')
+export class FooRequestErrorInterceptor{
+
+  constructor(fooInterceptorService) {
+    this.fooInterceptorService = fooInterceptorService;
+    this.objToExtend = {fromService: this.fooInterceptorService.getField()};
+  }
+
+  run(config) {
+    return Object.assign(config, this.objToExtend);
+  }
+}
+
+
+@service()
+export class FooInterceptorService {
+
+  constructor() {
+    this.field = "Hello from service"
+  }
+
+  getField() {
+    return this.field;
+  }
+}
+
+
+class FooInterceptor {
+
+  constructor(fooInterceptorService) {
+    this.fooInterceptorService = fooInterceptorService;
+    this.objToExtend = {fromService: this.fooInterceptorService.getField()};
+  }
+
+  run(config) {
+    return Object.assign(config, this.objToExtend);
+  }
+}
---
+++ test/interceptor.test.js
@@ -0,0 +1,64 @@
+import {flaky} from 'src/flaky';
+import {FooResponseInterceptor, FooRequestInterceptor, FooResponseErrorInterceptor, FooRequestErrorInterceptor} from './fixtures/FooInterceptor';
+
+describe('FooInterceptor', () => {
+  let $fooResponseInterceptor,
+    $fooRequestInterceptor,
+    $fooResponseErrorInterceptor,
+    $fooRequestErrorInterceptor;
+
+  let inputObj = {test: 'test'};
+  let expectedObj = {test: 'test', fromService: 'Hello from service'};
+
+  beforeEach(module('flakyApp'));
+
+  beforeEach(inject((_$fooResponseInterceptor_,
+                     _$fooRequestInterceptor_,
+                     _$fooResponseErrorInterceptor_,
+                     _$fooRequestErrorInterceptor_) => {
+
+    $fooResponseInterceptor = _$fooResponseInterceptor_;
+    $fooRequestInterceptor = _$fooRequestInterceptor_;
+    $fooResponseErrorInterceptor = _$fooResponseErrorInterceptor_;
+    $fooRequestErrorInterceptor = _$fooRequestErrorInterceptor_;
+  }));
+
+  it('All interceptors should be exists in flaky', () => {
+    let expectedArray = [
+      [FooResponseInterceptor, 'response'],
+      [FooRequestInterceptor, 'request'],
+      [FooResponseErrorInterceptor, 'responseError'],
+      [FooRequestErrorInterceptor, 'requestError']
+    ];
+    expect(flaky.module.interceptors).to.have.length(4);
+    expect(flaky.module.interceptors).to.deep.include.members(expectedArray);
+  });
+
+  it('All interceptors should be exists in angular', () => {
+    expect($fooResponseInterceptor).to.be.ok;
+    expect($fooRequestInterceptor).to.be.ok;
+    expect($fooResponseErrorInterceptor).to.be.ok;
+    expect($fooRequestErrorInterceptor).to.be.ok;
+  });
+
+  it('FooResponseInterceptor[\'response\'] should return updated argument', () => {
+    let responseInterceptor = $fooResponseInterceptor['response'];
+    expect(responseInterceptor(inputObj)).to.deep.equal(expectedObj);
+  });
+
+  it('FooRequestInterceptor[\'request\'] should return updated argument', () => {
+    let requestInterceptor = $fooRequestInterceptor['request'];
+    expect(requestInterceptor(inputObj)).to.deep.equal(expectedObj);
+  });
+
+  it('FooResponseErrorInterceptor[\'responseError\'] should return updated argument', () => {
+    let responseErrorInterceptor = $fooResponseErrorInterceptor['responseError'];
+    expect(responseErrorInterceptor(inputObj)).to.deep.equal(expectedObj);
+  });
+
+  it('FooRequestErrorInterceptor[\'requestError\'] should return updated argument', () => {
+    let requestErrorInterceptor = $fooRequestErrorInterceptor['requestError'];
+    expect(requestErrorInterceptor(inputObj)).to.deep.equal(expectedObj);
+  });
+
+});
