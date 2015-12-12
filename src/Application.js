87dd125ad03e11df3dad6bf00cacbf283d6dca57
Vitaly Lobchuk <vn.lobchuk@gmail.com>
2015-12-12 15:29:33 +0300

    Refatoring

--- src/Application.js
+++ src/Application.js
@@ -1,16 +1,11 @@
-import {injectRun, injectConfig} from './decorators';
 import {Module} from './Module';
+import {inject} from './decorators';
 
-@injectConfig('$locationProvider')
-@injectRun('$rootScope')
+@inject('$rootScope')
 export class Application extends Module {
 
   constructor(name) {
     super(name);
-    this._useHtml5Mode = true;
-    this._requireBase = true;
-    this._rewriteLinks = true;
-    this._urlPrefix = '!';
   }
 
   run(rootScope) {
@@ -25,36 +20,9 @@ export class Application extends Module {
     });
   }
 
-  config(stateProvider, httpProvider, locationProvider) {
-    super.config(stateProvider, httpProvider);
-
-    locationProvider.html5Mode({
-      'enabled': this._useHtml5Mode,
-      'requireBase': this._requireBase,
-      'rewriteLinks': this._rewriteLinks
-    }).hashPrefix(this._urlPrefix);
-  }
-
   onStateChangeStart(event, toState, toParams, fromState, fromParams) {
   }
 
   onStateChangeSuccess(event, toState, toParams, fromState, fromParams) {
-  }
-
-  setUseHtml5Mode(useHtml5Mode) {
-    this._useHtml5Mode = useHtml5Mode;
-    return this;
-  }
-
-  setRequireBase(requireBase) {
-    this._requireBase = requireBase;
-  }
-
-  setRewriteLinks(rewriteLinks) {
-    this._rewriteLinks = rewriteLinks;
-  }
-
-  setUrlPrefix(urlPrefix) {
-    this._urlPrefix = urlPrefix;
   }
 }
