289a25694206cef1c39b83ce7e219184415371c9
Vitaly Lobchuk <vn.lobchuk@gmail.com>
2015-12-12 15:24:39 +0300

    Disable Safari and Firefox

--- karma.conf.js
+++ karma.conf.js
@@ -87,7 +87,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
-      'Chrome', 'Firefox', 'Safari'
+      'Chrome'//, 'Firefox', 'Safari'
     ],
 
     // Continuous Integration mode
