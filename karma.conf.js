b6a480f036e6a3ddb8ebdaa3aa516570bedbbeb9
Vitaly Lobchuk <vn.lobchuk@gmail.com>
2015-12-13 23:58:03 +0300

    karma change browser to firefox

--- karma.conf.js
+++ karma.conf.js
@@ -87,7 +87,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: [
-      'Chrome'//, 'Firefox', 'Safari'
+      'Firefox'//, 'Chrome', 'Safari'
     ],
 
     // Continuous Integration mode
